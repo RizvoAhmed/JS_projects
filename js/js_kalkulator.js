015e4b0c83347556d1c54a75f7d3f50770938a1c
AhmedRizvo <ahmedrizvo99@gmail.com>
2022-12-06 17:19:03 +0100

    sitne izmjene

--- js/js_kalkulator.js
+++ js/js_kalkulator.js
@@ -1,21 +1,18 @@
 function inflationCalculator() {
-  let inflacionRate = document.querySelector("#inflationRate");
-  let money = document.querySelector("#money");
-  let years = document.querySelector("#years");
+  let inflationRate = document.getElementById("inflationRate");
+  let money = document.getElementById("money");
+  let years = document.getElementById("years");
 
-  inflacionRate = parseFloat(inflacionRate.value);
+  inflationRate = parseFloat(inflationRate.value);
   money = parseFloat(money.value);
   years = parseFloat(years.value);
 
-  let worth = money + money * (inflacionRate / 100);
+  let worth = money + money * (inflationRate / 100);
 
   for (let i = 1; i < years; i++) {
-    worth += worth * (inflacionRate / 100);
+    worth += worth * (inflationRate / 100);
   }
-
   worth = worth.toFixed(2);
-
-  console.log(worth);
 
   let newElement = document.createElement("div");
   newElement.className = "new-value";
